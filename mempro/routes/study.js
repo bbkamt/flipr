e71cc4e068634964eb12ece5258f7ee6430f6163
Alex Taylor <ataylo27@dcs.bbk.ac.uk>
2019-07-17 10:40:16 +0100

    cards now display question THEN answer when being reviewed

--- mempro/routes/study.js
+++ mempro/routes/study.js
@@ -1,5 +1,11 @@
-const {Card, validate} = require('../models/card');
+/*
+Flow of requests 
+i) User is decks page, selects deck to study and POST /api/study/q is called
+ii) User is shown question of card, clicks to reveal answer and POST /api/study/decks is called
+iii) User is shown quesiton, answer, and buttons to rate card, and POST /api/study/a is called
+*/
 
+const {Card, validate} = require('../models/card');
 const bodyParser = require('body-parser');
 const datetime = require('node-datetime');
 const express = require('express');
@@ -9,24 +15,12 @@ const router = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get('/', async (req, res) => {
-
-    updateAllDue();
-
-    let card = await Card.findOne({ due: true });
-    if (!card) return res.render('finished')
-    else {
-        card.current = true; 
-        card = await card.save();
-        console.log(req.body);
-        res.render('study', {
-            Question: card.question,
-            Answer: card.answer
-        })
-    }
-});
-
-router.post('/decks', async (req, res) => {
+/* 
+Post request that will return the question side of a card that 
+is due to be reviewed from the deck selected by the user from the decks
+page. 
+*/
+router.post('/q', async (req, res) => {
     console.log(req.body.deck);
     let card = await Card.findOne({ deck: req.body.deck, due: true });
     if (!card) return res.render('finished')
@@ -34,32 +28,19 @@ router.post('/decks', async (req, res) => {
         card.current = true; 
         card = await card.save();
         console.log(req.body);
-        res.render('study', {
-            Answer: card.answer,
+        res.render('studyQuestion', {
             Question: card.question,
             Deck: card.deck
         })
     }
 })
 
-
-router.post('/decks/answer', async (req, res) => {
-    console.log(req.body.deck);
-    let card = await Card.findOne({ deck: req.body.deck, due: true });
-    if (!card) return res.render('finished')
-    else {
-        card.current = true; 
-        card = await card.save();
-        console.log(req.body);
-        res.render('study', {
-            Question: card.question,
-            Answer: card.answer,
-            Deck: card.deck
-        })
-    }
-})
-
-router.post('/', async (req, res) => {
+/* 
+Post request that updates the card being reviewed with the 
+information provided by the user (difficulty rating 0-4). Saves
+the updated information and returns the next card due to be reviewed.
+*/
+router.post('/a', async (req, res) => {
     // const { error } = validate(req.body);
     // if (error) return res.status(400).send(error.details[0].message);
     let card = await Card.findOne({ current: true });
@@ -82,13 +63,56 @@ router.post('/', async (req, res) => {
     card = await card.save();
     console.log(card);
 
-    // get next card 
+    // get next card and display 
     card = await Card.findOne({ deck: card.deck, due: true });
     if (!card) return res.render('finished')
 
     else {
         card.current = true; 
         card = await card.save();
+        res.render('studyQuestion', {
+            Question: card.question,
+            Deck: card.deck
+        })
+    }
+});
+
+
+/* 
+Post request which returns the question and answer side of the 
+card being reviewed to the browser. 
+*/
+router.post('/decks', async (req, res) => {
+    console.log(req.body.deck);
+    let card = await Card.findOne({ deck: req.body.deck, due: true });
+    if (!card) return res.render('finished')
+    else {
+        card.current = true; 
+        card = await card.save();
+        console.log(req.body);
+        res.render('study', {
+            Answer: card.answer,
+            Question: card.question,
+            Deck: card.deck
+        })
+    }
+})
+
+
+/* 
+Get request - not currently in use. 
+*/
+router.get('/', async (req, res) => {
+
+    // Move this function call to a separate button on decks page 
+    updateAllDue();
+
+    let card = await Card.findOne({ due: true });
+    if (!card) return res.render('finished')
+    else {
+        card.current = true; 
+        card = await card.save();
+        console.log(req.body);
         res.render('study', {
             Question: card.question,
             Answer: card.answer
@@ -96,7 +120,13 @@ router.post('/', async (req, res) => {
     }
 });
 
+
 module.exports = router;
+
+
+/* 
+All functions used to update card information. 
+*/
 
 async function updateAllDue(){
     const date = parseInt(datetime.create().format('Ymd'));
