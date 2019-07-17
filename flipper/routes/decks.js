2638577da057b7e3b042d7c5dd94dffba71f8cdf
Alex Taylor <ataylo27@dcs.bbk.ac.uk>
2019-07-17 13:44:26 +0100

    decks appear sorted on decks page

--- flipper/routes/decks.js
+++ flipper/routes/decks.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
 
     let decks = await Deck.find().sort({ name: 1 });
     if (!decks) return res.status(400).send('You don\'t have any decks. Create one and try again.');
-    console.log(decks);
+    
     res.render('decks', {
         deck: decks
     });
